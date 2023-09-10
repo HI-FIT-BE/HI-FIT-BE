f78d7c5b3340bf918d2412ca73e1b4aa8b44e0a5
gwonwoo-nam <ngw7617@naver.com>
2023-09-10 14:48:41 +0900

    feat: 프로젝트 및 로그인 초기 설정

---
+++ src/main/java/hifit/be/user/dto/token/OauthToken.java
@@ -0,0 +1,21 @@
+package hifit.be.user.dto.token;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OauthToken {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long expiresIn;
+}
---
+++ src/main/java/hifit/be/user/service/UserService.java
@@ -0,0 +1,9 @@
+package hifit.be.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
